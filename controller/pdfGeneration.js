c5df9c2bf35b53ac366c6681b1398e47d250f023
ankitbihani415 <ankitbihani415@gmail.com>
2021-03-04 10:35:11 +0530

    work on pdf

--- controller/pdfGeneration.js
+++ controller/pdfGeneration.js
@@ -19,14 +19,20 @@ module.exports = {
         });
     },
     handleBar : (req,res) => {
-        var source = fs.readFileSync('./views/handlebartest.handlebars', 'utf8');
+        // var source = fs.readFileSync('./views/handlebartest.handlebars', 'utf8');
+        var source = fs.readFileSync('./views/print.handlebars', 'utf8');
         const template = Handlebars.compile(source);
         data = {
-            'name' : 'ankit',
-            'image1' : 'https://lh3.googleusercontent.com/-NwvTxqbH1mk/YD3XcsYQXmI/AAAAAAAACXU/kD91V1AnNYUiVm5uFBVkagz2KPqqocbBACK8BGAsYHg/s0/2021-03-01.png',
-            'image2' : 'https://d3gwru59h34lrx.cloudfront.net/creatives/4655/3510/tmp_2Ff0484d31_2F_241612942347429.jpeg'
+            created_at:'today',
+            owner_name : 'ankit',
+            image1 : 'https://lh3.googleusercontent.com/-NwvTxqbH1mk/YD3XcsYQXmI/AAAAAAAACXU/kD91V1AnNYUiVm5uFBVkagz2KPqqocbBACK8BGAsYHg/s0/2021-03-01.png',
+            image2 : 'https://d3gwru59h34lrx.cloudfront.net/creatives/4655/3510/tmp_2Ff0484d31_2F_241612942347429.jpeg'
         }
-        pdf.create(template(data)).toStream(function(err, stream){
+        pdf.create(template(data),{
+            format:'Legal',
+            orientation:'landscape',
+            base:'127.0.1:8000'
+        }).toStream(function(err, stream){
             stream.pipe(fs.createWriteStream('./hanlebarfoo.pdf'));
             res.status(200).send('check file')
         });
