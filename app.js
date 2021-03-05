9b6f870272165a7a3004835a29b4a6de579a0faf
ankitbihani415 <ankitbihani415@gmail.com>
2021-03-05 09:42:34 +0530

    work on email

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const dotenv = require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,15 +17,68 @@ var app = express();
 global.appRoot = path.resolve(__dirname);
 
 app.use('/course-graphql', graphqlHTTP({
-  schema: courseSchema,
-  rootValue: courseRoute,
-  graphiql: true,
+	schema: courseSchema,
+	rootValue: courseRoute,
+	graphiql: true,
 }));
 app.use('/hello-graphql', graphqlHTTP({
-  schema: helloSchema,
-  rootValue: helloRoot,
-  graphiql: true,
+	schema: helloSchema,
+	rootValue: helloRoot,
+	graphiql: true,
 }));
+
+app.get('/send-email',function(req,res){
+	// using Twilio SendGrid's v3 Node.js Library
+	// https://github.com/sendgrid/sendgrid-nodejs
+	const sgMail = require('@sendgrid/mail')
+	sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+	const msg = {
+		to: 'hkanwar@dgera.com', // Change to your recipient
+		from: 'ankitbihani415@gmail.com', // Change to your verified sender
+		subject: 'Sending with SendGrid is Fun',
+		text: 'and easy to do anywhere, even with Node.js',
+		html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+	}
+	sgMail
+    .send(msg)
+    .then(() => {
+		console.log('Email sent')
+    })
+    .catch((error) => {
+		console.error('error => ',error.response.body)
+    })
+  
+	res.send({msg:'check mail'})
+})
+
+app.get('/mailer',function(req,res){
+	const nodemailer = require('nodemailer');
+	const transporter = nodemailer.createTransport({
+		host: process.env.SMTP_SERVER,
+		port: process.env.SMTP_PORT,
+		auth: {
+			user: process.env.SMTP_USER,
+			pass: process.env.SMTP_PASS
+		},
+		secureConnection: false,
+		secure: false,
+		tls: { ciphers: 'SSLv3' },
+		requireTLS: true
+	});
+
+	// Send an email to support
+	var emailBody = {
+		from: process.env.SMTP_SENDER,
+		to: process.env.SUPPORT_EMAIL,
+		subject: 'Email test from nodemailer',
+		text: 'this is test email from node mailer',
+		html: '<p>node mail test successs.</p>'
+	};
+	transporter.sendMail(emailBody, function(err, info) {
+		if (err) { console.log('Error sending mail: ' + err) }
+	});
+	res.send({msg:'check mail'})
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
